beede9b0cd1df1a8202daeb52587f4968e8ebb13
LeoGidev <leogimenez.dev@gmail.com>
2024-05-30 23:46:53 -0300

    funcion lienas de fuga

--- lineapp.py
+++ lineapp.py
@@ -1,3 +1,15 @@
 import tkinter as tk
 from tkinter import Canvas
 import matplotlib.pyplot as plt
+
+class LineasDeFugaApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Líneas de Fuga para Bocetos de Ciudades")
+        
+        self.canvas = Canvas(root, width=800, height=600, bg="white")
+        self.canvas.pack()
+        
+        self.canvas.bind("<Button-1>", self.obtener_punto_fuga)
+        
+        self.punto_fuga = None
