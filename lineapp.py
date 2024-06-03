2569618b6349ff2ac52228b0797e2740b9fa4866
LeoGidev <leogimenez.dev@gmail.com>
2024-06-03 20:05:58 -0300

    se trabaj em nuevas funciones

--- lineapp.py
+++ lineapp.py
@@ -88,26 +88,26 @@ class LineasDeFugaApp:
             # Dibujar líneas de fuga horizontales
             for y in range(0, self.height_px+1, int(self.dpi * horizontal_spacing)):
                 self.canvas.create_line(0, y, x_fuga, y_fuga, fill=color, width=line_thickness)
-                self.canvas.create_line(self.width_px, y, x_fuga, y_fuga, fill=color, width=line_thickness)
+                self.canvas.create_line(self.width_px, y, x_fuga, y_fuga, fill="black", width=line_thickness)
             
             # Dibujar líneas de fuga verticales
             for x in range(0, self.width_px+1, int(self.dpi * vertical_spacing)):
                 self.canvas.create_line(x, 0, x_fuga, y_fuga, fill=color, width=line_thickness)
-                self.canvas.create_line(x, self.height_px, x_fuga, y_fuga, fill=color, width=line_thickness)
+                self.canvas.create_line(x, self.height_px, x_fuga, y_fuga, fill="red", width=line_thickness)
             
             # Dibujar líneas verdes horizontales debajo del punto de fuga dentro del triángulo
             for y in range(y_fuga, self.height_px+1, int(self.dpi * horizontal_spacing)):
                 left_intercept_x = x_fuga - (x_fuga * (y - y_fuga) / y_fuga)
                 right_intercept_x = x_fuga + ((self.width_px - x_fuga) * (y - y_fuga) / y_fuga)
                 if left_intercept_x >= 0 and right_intercept_x <= self.width_px:
-                    self.canvas.create_line(left_intercept_x, y, right_intercept_x, y, fill="green", width=line_thickness)
+                    self.canvas.create_line(left_intercept_x, y, right_intercept_x, y, fill="blue", width=line_thickness)
             
             # Dibujar líneas rosadas horizontales encima del punto de fuga dentro del triángulo
             for y in range(0, y_fuga, int(self.dpi * horizontal_spacing)):
                 left_intercept_x = x_fuga - (x_fuga * (y_fuga - y) / y_fuga)
                 right_intercept_x = x_fuga + ((self.width_px - x_fuga) * (y_fuga - y) / y_fuga)
                 if left_intercept_x >= 0 and right_intercept_x <= self.width_px:
-                    self.canvas.create_line(left_intercept_x, y, right_intercept_x, y, fill="pink", width=line_thickness)
+                    self.canvas.create_line(left_intercept_x, y, right_intercept_x, y, fill="blue ligth", width=line_thickness)
 
     def cambiar_fondo(self):
         color = colorchooser.askcolor()[1]
